5b73d0f29fe8edbb2d8ebb7cd678ba48431c323a
philipe_kraken <philipeacampos@hotmail.com>
2022-06-21 21:48:32 -0300

    Created configuration about connect with SqlServer

--- ControleDeContatos/Data/BancoContext.cs
+++ ControleDeContatos/Data/BancoContext.cs
@@ -1,7 +1,16 @@
-﻿namespace ControleDeContatos.Data
-{
-    public class BancoContext
-    {
+﻿using ControleDeContatos.Models;
+using Microsoft.EntityFrameworkCore;
 
+namespace ControleDeContatos.Data
+{
+    public class BancoContext : DbContext
+    {
+        //Configuração Padrao do SqlServer
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ContatoModel> Contatos { get; set; }
     }
 }
