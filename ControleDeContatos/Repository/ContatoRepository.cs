ee0107962848347d0f3f01f76e0de34c1909424e
philipe_kraken <philipeacampos@hotmail.com>
2022-06-22 15:32:44 -0300

    Init class Repository for conncet methods for database

---
+++ ControleDeContatos/Repository/ContatoRepository.cs
@@ -0,0 +1,23 @@
+﻿using ControleDeContatos.Data;
+using ControleDeContatos.Models;
+
+namespace ControleDeContatos.Repository
+{
+    public class ContatoRepository : IContatoRepository
+    {
+        private readonly BancoContext _bancoContext;
+
+        public ContatoRepository(BancoContext bancoContext)
+        {
+            _bancoContext = bancoContext;
+        }
+
+        public ContatoModel Adicionar(ContatoModel contato)
+        {
+            //Gravar no Banco de Dados
+            _bancoContext.Contatos.Add(contato);
+            _bancoContext.SaveChanges();
+;            return contato;
+        }
+    }
+}
